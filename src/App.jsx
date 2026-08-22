9a51003d5d269e5ae6b43e43304455670f7c4b89
Kevin Stif Sánchez Urbina <67239585+kevinstif@users.noreply.github.com>
2026-08-22 12:47:36 -0500

    feat: migrate to vite/react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Portfolio from './pages/Portfolio'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Portfolio />
+  )
+}
+
+export default App
---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,11 @@
+const Portfolio = () => {
+  return (
+    <>
+     
+
+    </>
+
+  );
+};
+
+export default Portfolio;
