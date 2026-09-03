3daafe803fed601a66b61454f549488f0b9d4f75
Kevin Stif Sánchez Urbina <67239585+kevinstif@users.noreply.github.com>
2026-09-03 18:36:51 -0500

    fix(projects): images

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ export const main = {
   title: "Teacher Finder",
   status: "Finalizado",
   description:
-    "Teacher Finder simplifica la búsqueda de empleo para docentes y la contratación de talento para instituciones educativas. Además, los profesores pueden crear un video de presentación que recibe análisis de IA para su mejora. Fue, desarrollada por un equipo de 8 personas para su versión web y móvil, participé en el equipo de desarrollo backend conformado por 2 personas.",
+    "Teacher Finder simplifica la búsqueda de empleo para docentes y la contratación de talento para instituciones educativas. Además, los profesores pueden crear un video de presentación que recibe análisis de IA para su mejora. Fue desarrollada por un equipo de 8 personas para su versión web y móvil, participé en el equipo de desarrollo backend conformado por 2 personas.",
   technologies: [
     { name: "Angular", icon: "devicon-angularjs-plain colored" },
     { name: "Spring", icon: "devicon-spring-original colored" },
@@ -10,15 +10,18 @@ export const main = {
     { name: "TypeScript", icon: "devicon-typescript-plain colored" },
     { name: "Java", icon: "devicon-java-plain colored" },
   ],
-  image:
-    "https://kevinstif.github.io/Portfolio/resource/images/teacher-finder-img.png",
+  image: "/Portfolio/projects/teacher-finder.webp",
   links: {
     live: "https://arquitectura-de-software-emergentes.github.io/Landing-Page",
     github: {
-      landing: "https://github.com/Arquitectura-De-Software-Emergentes/Landing-Page",
-      frontend: "https://github.com/Arquitectura-De-Software-Emergentes/Frontend",
-      backend: "https://github.com/Arquitectura-De-Software-Emergentes/Backend",
-      mobile: "https://github.com/Arquitectura-De-Software-Emergentes/Mobile"
+      landing:
+        "https://github.com/Arquitectura-De-Software-Emergentes/Landing-Page",
+      frontend:
+        "https://github.com/Arquitectura-De-Software-Emergentes/Frontend",
+      backend:
+        "https://github.com/Arquitectura-De-Software-Emergentes/Backend",
+      mobile:
+        "https://github.com/Arquitectura-De-Software-Emergentes/Mobile",
     },
   },
 };
